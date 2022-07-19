3200870c1f2838af6d5b544d9601f6160e98e6af
keremkundak <keremkundak@hotmail.com>
2022-07-20 01:08:37 +0300

    Add files via upload
    Solution of problem1.

---
+++ problem1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+    int i, sum = 0;
+    for(i = 1; i < 1000; i++) // go through numbers from 1 to 1000
+        if(i % 3 == 0 || i % 5 == 0 )// if the number is multiple of 3 or 5
+            sum += i; // add it to sum
+    printf("%d\n", sum); // print the answer 
+    return 0;
+}
